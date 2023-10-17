d41363876a2a8cd1b7fe8440cf065e236ee6fe14
Adriano Pessoa <adrianobpessoa@gmail.com>
2023-10-17 01:36:16 -0300

    feat: create reducer and transitions to question

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react'
 import './App.css'
 import Welcome from './components/Welcome'
+import Question from './components/Question'
+import { QuizContext } from './context/quiz'
 
 function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
       <div className='App'>
         <h1>Quiz de programação</h1>
-        <Welcome/>
+        {quizState.gameStage === "Start" && <Welcome/>}
+        {quizState.gameStage === "Playng" && <Question/>}
       </div>
   )
 }
